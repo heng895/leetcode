ae450567e5f4e6d0004b0762d701ce4facf83ebc
heng <2530439948@qq.com>
2024-05-09 15:46:06 +0800

    20240509

--- src/main/java/com/henry/leetcode/editor/cn/T1_TwoSum.java
+++ src/main/java/com/henry/leetcode/editor/cn/T1_TwoSum.java
@@ -67,10 +67,10 @@ public class T1_TwoSum {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int[] twoSum(int[] nums, int target) {
-            Map<Integer,Integer> map = new HashMap<>();
+            Map<Integer, Integer> map = new HashMap<>();
             for (int i = 0; i < nums.length; i++) {
-                if(map.containsKey(target-nums[i])) return new int[]{map.get(target-nums[i]),i};
-                map.put(nums[i],i);
+                if (map.containsKey(target - nums[i])) return new int[]{map.get(target - nums[i]), i};
+                map.put(nums[i], i);
             }
             return new int[0];
         }
