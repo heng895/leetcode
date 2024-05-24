73a18c50d2fab32ee7e8d485a77693fa0e95f57e
heng <2530439948@qq.com>
2024-05-24 22:52:12 +0800

    202405024

---
+++ src/main/java/com/henry/leetcode/editor/cn/T17_LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,85 @@
+//给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。 
+//
+// 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。 
+//
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：digits = "23"
+//输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：digits = ""
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：digits = "2"
+//输出：["a","b","c"]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= digits.length <= 4 
+// digits[i] 是范围 ['2', '9'] 的一个数字。 
+// 
+//
+// Related Topics 哈希表 字符串 回溯 👍 2823 👎 0
+
+
+package com.henry.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Henry
+ * @date 2024-05-24 15:05:41
+ */
+//Java：电话号码的字母组合
+public class T17_LetterCombinationsOfAPhoneNumber {
+    public static void main(String[] args) {
+        Solution solution = new T17_LetterCombinationsOfAPhoneNumber().new Solution();
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        List<String> res = new ArrayList<>();
+        StringBuilder path = new StringBuilder();
+
+        public List<String> letterCombinations(String digits) {
+            if (digits.length() == 0) return res;
+            String[] numString = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+            backtracking(digits, numString, 0);
+            return res;
+        }
+
+        public void backtracking(String digits, String[] numString, int start) {
+            if (digits.length() == start) {
+                res.add(path.toString());
+                return;
+            }
+            String str = numString[digits.charAt(start) - '0'];
+            for (int i = 0; i < str.length(); i++) {
+                path.append(str.charAt(i));
+                backtracking(digits, numString, start + 1);
+                path.deleteCharAt(path.length() - 1);
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
